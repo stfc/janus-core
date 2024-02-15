1b0571202df28cb7c3f9a7fcb38f6ed81ab54f5b
ElliottKasoar <ElliottKasoar@users.noreply.github.com>
2024-02-15 00:48:02 +0000

    Add assertion for example test

---
+++ tests/test_janus.py
@@ -0,0 +1,12 @@
+"""
+Test importing janus_core.
+"""
+
+import janus_core
+
+
+def test_import():
+    """
+    Test janus_core has been imported successfully.
+    """
+    assert janus_core.__version__ is not None
