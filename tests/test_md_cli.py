a28958676b3d44f7c399cab995b61867dec6a6de
ElliottKasoar <ElliottKasoar@users.noreply.github.com>
2024-03-26 11:04:01 +0000

    Update MD CLI tests

--- tests/test_md_cli.py
+++ tests/test_md_cli.py
@@ -2,6 +2,8 @@
 
 from pathlib import Path
 
+from ase import Atoms
+from ase.io import read
 import pytest
 from typer.testing import CliRunner
 
@@ -31,8 +33,9 @@ test_data = [
 
 @pytest.mark.parametrize("ensemble", test_data)
 def test_md(ensemble, tmp_path):
-    """Test singlepoint calculation."""
+    """Test all MD simulations are able to run."""
     file_prefix = tmp_path / f"{ensemble}-T300"
+    traj_path = tmp_path / f"{ensemble}-T300-traj.xyz"
 
     result = runner.invoke(
         app,
@@ -45,8 +48,39 @@ def test_md(ensemble, tmp_path):
             300,
             "--file-prefix",
             file_prefix,
+            "--steps",
+            2,
+            "--traj-every",
+            1,
         ],
     )
 
-    # atoms = read_atoms(results_path)
     assert result.exit_code == 0
+
+    # Check at least one image has been saved in trajectory
+    atoms = read(traj_path)
+    assert isinstance(atoms, Atoms)
+
+
+def test_md_log(tmp_path, caplog):
+    """Test log correctly written for MD."""
+    file_prefix = tmp_path / "nvt-T300"
+
+    with caplog.at_level("INFO", logger="janus_core.md"):
+        result = runner.invoke(
+            app,
+            [
+                "md",
+                "nvt",
+                "--struct",
+                DATA_PATH / "NaCl.cif",
+                "--temp",
+                300,
+                "--file-prefix",
+                file_prefix,
+                "--steps",
+                2,
+            ],
+        )
+        assert result.exit_code == 0
+        assert " Starting molecular dynamics simulation" in caplog.text
