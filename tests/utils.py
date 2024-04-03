ea36f172bdb5bc4fbb60505d5bc7ba0197fd8361
ElliottKasoar <ElliottKasoar@users.noreply.github.com>
2024-04-03 17:35:16 +0100

    Refactor log checking

--- tests/utils.py
+++ tests/utils.py
@@ -1,10 +1,13 @@
 """Utility functions for tests."""
 
 from pathlib import Path
-from typing import Union
+from typing import Optional, Union
 
 from ase import Atoms
 from ase.io import read
+import yaml
+
+from janus_core.janus_types import MaybeSequence, PathLike
 
 
 def read_atoms(path: Path) -> Union[Atoms, None]:
@@ -30,3 +33,37 @@ def read_atoms(path: Path) -> Union[Atoms, None]:
         path.unlink()
 
     return atoms if atoms else None
+
+
+def check_log_contents(
+    log_path: PathLike,
+    contains: Optional[MaybeSequence[str]] = None,
+    excludes: Optional[MaybeSequence[str]] = None,
+) -> None:
+    """
+    Check messages are present or not within a yaml-formatted log file.
+
+    Parameters
+    ----------
+    log_path : PathLike
+        Path to log file to check messsages of.
+    contains : MaybeSequence[str]
+        Messages that must appear in the log file. Default is None.
+    excludes : MaybeSequence[str]
+        Messages that must not appear in the log file. Default is None.
+    """
+    # Convert single strings to iterable
+    contains = [contains] if isinstance(contains, str) else contains
+    excludes = [excludes] if isinstance(excludes, str) else excludes
+
+    # Read log file
+    with open(log_path, encoding="utf8") as log_file:
+        logs = yaml.safe_load(log_file)
+    messages = "".join(log["message"] for log in logs)
+
+    if contains:
+        for msg in contains:
+            assert msg in messages
+    if excludes:
+        for msg in excludes:
+            assert msg not in messages
