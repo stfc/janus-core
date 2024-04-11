3bff37e840bca67ff1ffc3f79f2c7ded70bd3fbf
ElliottKasoar <ElliottKasoar@users.noreply.github.com>
2024-04-11 15:50:58 +0100

    Convert yaml configuration hyphens to underscores

--- janus_core/utils.py
+++ janus_core/utils.py
@@ -1,5 +1,6 @@
 """Utility functions for janus_core."""
 
+from pathlib import Path
 from typing import Optional
 
 
@@ -20,3 +21,39 @@ def none_to_dict(dictionaries: list[Optional[dict]]) -> list[dict]:
     for i, dictionary in enumerate(dictionaries):
         dictionaries[i] = dictionary if dictionary else {}
     return dictionaries
+
+
+def dict_paths_to_strs(dictionary: dict) -> None:
+    """
+    Recursively iterate over dictionary, converting Path values to strings.
+
+    Parameters
+    ----------
+    dictionary : dict
+        Dictionary to be converted.
+    """
+    for key, value in dictionary.items():
+        if isinstance(value, dict):
+            dict_paths_to_strs(value)
+        elif isinstance(value, Path):
+            dictionary[key] = str(value)
+
+
+def dict_remove_hyphens(dictionary: dict) -> dict:
+    """
+    Recursively iterate over dictionary, replacing hyphens with underscores in keys.
+
+    Parameters
+    ----------
+    dictionary : dict
+        Dictionary to be converted.
+
+    Returns
+    -------
+    dict
+        Dictionary with hyphens in keys replaced with underscores.
+    """
+    for key, value in dictionary.items():
+        if isinstance(value, dict):
+            dictionary[key] = dict_remove_hyphens(value)
+    return {k.replace("-", "_"): v for k, v in dictionary.items()}
---
+++ tests/test_utils.py
@@ -0,0 +1,48 @@
+"""Test utility functions."""
+
+from pathlib import Path
+
+from janus_core.utils import dict_paths_to_strs, dict_remove_hyphens
+
+
+def test_dict_paths_to_strs():
+    """Test Paths are converted to strings."""
+    dictionary = {
+        "key1": Path("/example/path"),
+        "key2": {
+            "key3": Path("another/example"),
+            "key4": "example",
+        },
+    }
+
+    # Check Paths are present
+    assert isinstance(dictionary["key1"], Path)
+    assert isinstance(dictionary["key2"]["key3"], Path)
+    assert not isinstance(dictionary["key1"], str)
+
+    dict_paths_to_strs(dictionary)
+
+    # Check Paths are now strings
+    assert isinstance(dictionary["key1"], str)
+    assert isinstance(dictionary["key2"]["key3"], str)
+
+
+def test_dict_remove_hyphens():
+    """Test hyphens are replaced with underscores."""
+    dictionary = {
+        "key-1": "value_1",
+        "key-2": {
+            "key-3": "value-3",
+            "key-4": 4,
+            "key_5": 5.0,
+            "key6": {"key-7": "value7"},
+        },
+    }
+    dictionary = dict_remove_hyphens(dictionary)
+
+    # Check hyphens are now strings
+    assert dictionary["key_1"] == "value_1"
+    assert dictionary["key_2"]["key_3"] == "value-3"
+    assert dictionary["key_2"]["key_4"] == 4
+    assert dictionary["key_2"]["key_5"] == 5.0
+    assert dictionary["key_2"]["key6"]["key_7"] == "value7"
