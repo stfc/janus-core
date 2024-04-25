bb8f2dbc75e3a59053f1b81f1d4fe99aec5df52f
Jacob Wilkins <46597752+oerc0122@users.noreply.github.com>
2024-04-25 20:53:07 +0100

    Add a getitem to stats to retrieve data by labels or indices (#122)
    * Add a getitem to stats to retrieve data by labels or indices on the object itself
    
    * Make tests pytest parametrized

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,7 +38,7 @@ numpydoc_validation_checks = {"all", "EX01", "SA01", "ES01"}
 numpydoc_validation_exclude = {
     r"\.__weakref__$",
     r"\.__repr__$",
-    r"janus_core\.janus_types",
+    r"janus_core\.helpers\.janus_types",
 }
 numpydoc_class_members_toctree = False
 
@@ -192,4 +192,6 @@ html_search_language = "en"
 nitpick_ignore = [
     ("py:class", "Logger"),
     ("py:class", "numpy.float64"),
+    ("py:class", "ellipsis"),
+    ("py:class", "janus_core.helpers.stats.T"),
 ]
