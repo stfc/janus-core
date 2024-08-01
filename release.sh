8bcac724372dfa6b8c8eeaa5933f64e11a436fb5
ElliottKasoar <45317199+ElliottKasoar@users.noreply.github.com>
2024-08-01 18:47:34 +0100

    Add version for sevennet (#238)
    * Add version for sevennet
    
    * format
    
    ---------
    
    Co-authored-by: alin m elena <alinm.elena@gmail.com>

--- release.sh
+++ release.sh
@@ -4,8 +4,8 @@ version=$1
 
 sed -i "s;^version =.*;version = \"$version\";g" pyproject.toml
 
-git add pyproject.toml 
+git add pyproject.toml
 git commit -m "bump version for release $version"
 git tag -f -a v$version -m "release $version"
-git push 
-git push --tags 
+git push
+git push --tags
