6352656243a38193bf2bd4818bb9e422f799cded
ElliottKasoar <45317199+ElliottKasoar@users.noreply.github.com>
2024-08-09 17:21:07 +0100

    Update documentation for new pre-commit

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -74,7 +74,6 @@ copyright_year_string = (
     if current_year == copyright_first_year
     else f"{copyright_first_year}-{current_year}"
 )
-# pylint: disable=redefined-builtin
 copyright = f"{copyright_year_string}, {copyright_owners}. All rights reserved"
 
 # The version info for the project you're documenting, acts as replacement for
