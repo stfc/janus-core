94b6f0921bffbc28c48bdc6984af84bf24ac88e3
ElliottKasoar <45317199+ElliottKasoar@users.noreply.github.com>
2025-04-10 17:15:56 +0100

    Fix Orb calculator type

--- janus_core/helpers/mlip_calculators.py
+++ janus_core/helpers/mlip_calculators.py
@@ -264,22 +264,22 @@ def choose_calculator(
     elif arch == "orb":
         from orb_models import __version__
         from orb_models.forcefield.calculator import ORBCalculator
-        from orb_models.forcefield.graph_regressor import GraphRegressor
+        from orb_models.forcefield.direct_regressor import DirectForcefieldRegressor
         import orb_models.forcefield.pretrained as orb_ff
 
         # Default model
         model_path = model_path if model_path else "orb_v2"
 
-        if isinstance(model_path, GraphRegressor):
+        if isinstance(model_path, DirectForcefieldRegressor):
             model = model_path
-            model_path = "loaded_GraphRegressor"
+            model_path = "loaded_DirectForcefieldRegressor"
         else:
             try:
                 model = getattr(orb_ff, model_path.replace("-", "_"))()
             except AttributeError as e:
                 raise ValueError(
-                    "`model_path` must be a `GraphRegressor`, pre-trained model label "
-                    "(e.g. 'orb-v2'), or `None` (uses default, orb-v2)"
+                    "`model_path` must be a `DirectForcefieldRegressor`, pre-trained "
+                    "model label (e.g. 'orb-v2'), or `None` (uses default, orb-v2)"
                 ) from e
 
         calculator = ORBCalculator(model=model, device=device, **kwargs)
