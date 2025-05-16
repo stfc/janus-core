8bee59224ef20949f4cee4c591a8f904575849a0
ElliottKasoar <45317199+ElliottKasoar@users.noreply.github.com>
2025-05-16 11:38:01 +0100

    Rename filter parameter

--- janus_core/calculations/geom_opt.py
+++ janus_core/calculations/geom_opt.py
@@ -73,11 +73,11 @@ class GeomOpt(BaseCalculation):
     angle_tolerance
         Angle precision for spglib symmetry determination, in degrees. Default is -1.0,
         which means an internally optimized routine is used to judge symmetry.
-    filter_func
-        Filter function, or name of function from ase.filters to apply constraints to
-        atoms. Default is `FrechetCellFilter`.
+    filter_class
+        Filter class, or name of class from ase.filters to wrap around atoms.
+        Default is `FrechetCellFilter`.
     filter_kwargs
-        Keyword arguments to pass to filter_func. Default is {}.
+        Keyword arguments to pass to filter_class. Default is {}.
     optimizer
         Optimization function, or name of function from ase.optimize. Default is
         `LBFGS`.
@@ -114,7 +114,7 @@ class GeomOpt(BaseCalculation):
         symmetrize: bool = False,
         symmetry_tolerance: float = 0.001,
         angle_tolerance: float = -1.0,
-        filter_func: Callable | str | None = FrechetCellFilter,
+        filter_class: Callable | str | None = FrechetCellFilter,
         filter_kwargs: dict[str, Any] | None = None,
         optimizer: Callable | str = LBFGS,
         opt_kwargs: ASEOptArgs | None = None,
@@ -167,11 +167,11 @@ class GeomOpt(BaseCalculation):
         angle_tolerance
             Angle precision for spglib symmetry determination, in degrees. Default is
             -1.0, which means an internally optimized routine is used to judge symmetry.
-        filter_func
-            Filter function, or name of function from ase.filters to apply constraints
-            to atoms. Default is `FrechetCellFilter`.
+        filter_class
+            Filter class, or name of class from ase.filters to wrap around atoms.
+            Default is `FrechetCellFilter`.
         filter_kwargs
-            Keyword arguments to pass to filter_func. Default is {}.
+            Keyword arguments to pass to filter_class. Default is {}.
         optimizer
             Optimization function, or name of function from ase.optimize. Default is
             `LBFGS`.
@@ -200,7 +200,7 @@ class GeomOpt(BaseCalculation):
         self.symmetrize = symmetrize
         self.symmetry_tolerance = symmetry_tolerance
         self.angle_tolerance = angle_tolerance
-        self.filter_func = filter_func
+        self.filter_class = filter_class
         self.filter_kwargs = filter_kwargs
         self.optimizer = optimizer
         self.opt_kwargs = opt_kwargs
@@ -289,13 +289,13 @@ class GeomOpt(BaseCalculation):
         if self.logger:
             self.logger.info("Using optimizer: %s", self.optimizer.__name__)
 
-        if self.filter_func is not None:
+        if self.filter_class is not None:
             if "scalar_pressure" in self.filter_kwargs:
                 self.filter_kwargs["scalar_pressure"] *= units.GPa
-            self.filtered_struct = self.filter_func(self.struct, **self.filter_kwargs)
+            self.filtered_struct = self.filter_class(self.struct, **self.filter_kwargs)
             self.dyn = self.optimizer(self.filtered_struct, **self.opt_kwargs)
             if self.logger:
-                self.logger.info("Using filter: %s", self.filter_func.__name__)
+                self.logger.info("Using filter: %s", self.filter_class.__name__)
                 if "hydrostatic_strain" in self.filter_kwargs:
                     self.logger.info(
                         "hydrostatic_strain: %s",
@@ -314,18 +314,18 @@ class GeomOpt(BaseCalculation):
             self.dyn = self.optimizer(self.struct, **self.opt_kwargs)
 
     def _set_functions(self) -> None:
-        """Set optimizer and filter functions."""
+        """Set optimizer and filter."""
         if isinstance(self.optimizer, str):
             try:
                 self.optimizer = getattr(ase.optimize, self.optimizer)
             except AttributeError as e:
                 raise AttributeError(f"No such optimizer: {self.optimizer}") from e
 
-        if self.filter_func is not None and isinstance(self.filter_func, str):
+        if self.filter_class is not None and isinstance(self.filter_class, str):
             try:
-                self.filter_func = getattr(filters, self.filter_func)
+                self.filter_class = getattr(filters, self.filter_class)
             except AttributeError as e:
-                raise AttributeError(f"No such filter: {self.filter_func}") from e
+                raise AttributeError(f"No such filter: {self.filter_class}") from e
 
     def run(self) -> None:
         """Run geometry optimization."""
@@ -344,7 +344,7 @@ class GeomOpt(BaseCalculation):
         converged = self.dyn.run(fmax=self.fmax, steps=self.steps)
 
         # Calculate current maximum force
-        if self.filter_func is not None:
+        if self.filter_class is not None:
             max_force = linalg.norm(self.filtered_struct.get_forces(), axis=1).max()
         else:
             max_force = linalg.norm(self.struct.get_forces(), axis=1).max()
@@ -355,7 +355,7 @@ class GeomOpt(BaseCalculation):
             # Update max force
             old_max_force = max_force
             struct = (
-                self.filtered_struct if self.filter_func is not None else self.struct
+                self.filtered_struct if self.filter_class is not None else self.struct
             )
             max_force = linalg.norm(struct.get_forces(), axis=1).max()
 
--- janus_core/cli/geomopt.py
+++ janus_core/cli/geomopt.py
@@ -42,7 +42,7 @@ def _set_minimize_kwargs(
         Other keyword arguments to pass to geometry optimizer.
     opt_cell_lengths
         Whether to optimize cell vectors, as well as atomic positions, by setting
-        `hydrostatic_strain` in the filter function.
+        `hydrostatic_strain` in the filter.
     pressure
         Scalar pressure when optimizing cell geometry, in GPa. Passed to the filter
         function if either `opt_cell_lengths` or `opt_cell_fully` is True.
@@ -113,11 +113,11 @@ def geomopt(
             rich_help_panel="Calculation",
         ),
     ] = False,
-    filter_func: Annotated[
+    filter_class: Annotated[
         str | None,
         Option(
             help=(
-                "Name of ASE filter/constraint function to use. If using "
+                "Name of ASE filter to wrap around atoms. If using "
                 "--opt-cell-lengths or --opt-cell-fully, defaults to "
                 "`FrechetCellFilter`."
             ),
@@ -199,14 +199,13 @@ def geomopt(
         Set maximum number of optimization steps to run. Default is 1000.
     opt_cell_lengths
         Whether to optimize cell vectors, as well as atomic positions, by setting
-        `hydrostatic_strain` in the filter function. Default is False.
+        `hydrostatic_strain` in the filter. Default is False.
     opt_cell_fully
         Whether to fully optimize the cell vectors, angles, and atomic positions.
         Default is False.
-    filter_func
-        Name of filter function from ase.filters or ase.constraints, to apply
-        constraints to atoms. If using --opt-cell-lengths or --opt-cell-fully, defaults
-        to `FrechetCellFilter` if available, otherwise `ExpCellFilter`.
+    filter_class
+        Name of filter from ase.filters to wrap around atoms. If using
+        --opt-cell-lengths or --opt-cell-fully, defaults to `FrechetCellFilter`.
     pressure
         Scalar pressure when optimizing cell geometry, in GPa. Passed to the filter
         function if either `opt_cell_lengths` or `opt_cell_fully` is True. Default is
@@ -293,16 +292,15 @@ def geomopt(
     _set_minimize_kwargs(minimize_kwargs, opt_cell_lengths, pressure)
 
     if opt_cell_fully or opt_cell_lengths:
-        # Use default filter unless filter function explicitly passed
-        opt_cell_fully_dict = {"filter_func": filter_func} if filter_func else {}
+        # Use default filter unless filter explicitly passed
+        opt_cell_fully_dict = {"filter_class": filter_class} if filter_class else {}
     else:
-        if filter_func:
+        if filter_class:
             raise ValueError(
-                "--opt-cell-lengths or --opt-cell-fully must be set to use a filter "
-                "function"
+                "--opt-cell-lengths or --opt-cell-fully must be set to use a filter"
             )
-        # Override default filter function with None
-        opt_cell_fully_dict = {"filter_func": None}
+        # Override default filter class with None
+        opt_cell_fully_dict = {"filter_class": None}
 
     log_kwargs = {"filemode": "w"}
     if log:
