24a4d0bbba4bd5a202ef17470d0300a860663c36
ElliottKasoar <45317199+ElliottKasoar@users.noreply.github.com>
2025-12-09 14:56:17 +0000

    Remove deprecated filter_func

--- janus_core/calculations/geom_opt.py
+++ janus_core/calculations/geom_opt.py
@@ -76,8 +76,6 @@ class GeomOpt(BaseCalculation):
     filter_class
         Filter class, or name of class from ase.filters to wrap around atoms.
         Default is `FrechetCellFilter`.
-    filter_func
-        Deprecated. Please use `filter_class`.
     filter_kwargs
         Keyword arguments to pass to filter_class. Default is {}.
     optimizer
@@ -117,7 +115,6 @@ class GeomOpt(BaseCalculation):
         symmetry_tolerance: float = 0.001,
         angle_tolerance: float = -1.0,
         filter_class: Callable | str | None = FrechetCellFilter,
-        filter_func: Callable | str | None = None,
         filter_kwargs: dict[str, Any] | None = None,
         optimizer: Callable | str = LBFGS,
         opt_kwargs: ASEOptArgs | None = None,
@@ -173,8 +170,6 @@ class GeomOpt(BaseCalculation):
         filter_class
             Filter class, or name of class from ase.filters to wrap around atoms.
             Default is `FrechetCellFilter`.
-        filter_func
-            Deprecated. Please use `filter_class`.
         filter_kwargs
             Keyword arguments to pass to filter_class. Default is {}.
         optimizer
@@ -199,10 +194,6 @@ class GeomOpt(BaseCalculation):
                 read_kwargs, filter_kwargs, opt_kwargs, write_kwargs, traj_kwargs
             )
         )
-
-        # Handle deprecated filter_func, but warn later after logging is set up
-        if filter_func:
-            filter_class = filter_func
 
         self.fmax = fmax
         self.steps = steps
@@ -241,15 +232,6 @@ class GeomOpt(BaseCalculation):
 
         if not self.struct.calc:
             raise ValueError("Please attach a calculator to `struct`.")
-
-        # Warn for deprecated filter_func now that logging is set up
-        if filter_func:
-            warnings.warn(
-                "`filter_func` has been deprecated, but currently overrides "
-                "`filter_class`. Please only set `filter_class`.",
-                FutureWarning,
-                stacklevel=2,
-            )
 
         # Set output files
         self.write_kwargs["filename"] = self._build_filename(
