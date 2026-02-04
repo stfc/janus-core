d009f46b1f61bb1dae53f0d91f37f8a953160d93
Harvey Devereux <h.devereux@qmul.ac.uk>
2026-02-04 10:40:49 +0000

    Add Sevennet foundation download

--- tests/models/extra_models.py
+++ tests/models/extra_models.py
@@ -14,7 +14,13 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     args.path.mkdir(parents=True, exist_ok=True)
+
     urlretrieve(
         "https://zenodo.org/records/16980200/files/NequIP-MP-L-0.1.nequip.zip",
         filename=args.path / "NequIP-MP-L-0.1.nequip.zip",
+    )
+
+    urlretrieve(
+        "https://github.com/MDIL-SNU/SevenNet/raw/dff008ac9c53d368b5bee30a27fa4bdfd73f19b2/sevenn/pretrained_potentials/SevenNet_l3i5/checkpoint_l3i5.pth",
+        filename=args.path / "SevenNet_l3i5.pth",
     )
