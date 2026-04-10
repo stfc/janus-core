6a22cc9ceb6d17680262085f00f0d2fc1fcc1727
Alin M Elena <alin@elena.re>
2026-04-10 15:55:14 +0100

    turn codecarbon error to a warning

--- janus_core/helpers/log.py
+++ janus_core/helpers/log.py
@@ -206,7 +206,7 @@ def config_tracker(
             carbon_logger.removeHandler(carbon_logger.handlers[0])
 
         if not hasattr(tracker, "_emissions"):
-            raise ValueError(
+            raise Warning(
                 "Carbon tracker has not been configured correctly. Please try "
                 "reconfiguring, or disable the tracker."
             )
