d70d203888f642f72ebc0fa5d7e9f9f26179f0b3
Harvey Devereux <33522054+harveydevereux@users.noreply.github.com>
2026-04-10 16:03:40 +0100

    Add SevenNet to training (#662)
    Co-authored-by: Jacob Wilkins <46597752+oerc0122@users.noreply.github.com>
    Co-authored-by: Elliott Kasoar <45317199+ElliottKasoar@users.noreply.github.com>

--- tests/models/extra_models.py
+++ tests/models/extra_models.py
@@ -5,6 +5,24 @@ from __future__ import annotations
 from argparse import ArgumentParser
 from pathlib import Path
 from urllib.request import urlretrieve
+from warnings import warn
+
+
+def try_retrieve(url: str, filename: Path):
+    """Attempt to retrieve from url.
+
+    Patameters
+    ----------
+    url
+        The url to attempt to retrieve from.
+    filename
+        The local filename of the retrieved data.
+    """
+    try:
+        urlretrieve(url, filename)
+    except Exception as e:
+        warn(f"Unable to retrieve from {url} because of:\n  {e}", stacklevel=2)
+
 
 if __name__ == "__main__":
     parser = ArgumentParser(
@@ -14,7 +32,13 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     args.path.mkdir(parents=True, exist_ok=True)
-    urlretrieve(
+
+    try_retrieve(
         "https://zenodo.org/records/16980200/files/NequIP-MP-L-0.1.nequip.zip",
-        filename=args.path / "NequIP-MP-L-0.1.nequip.zip",
+        args.path / "NequIP-MP-L-0.1.nequip.zip",
+    )
+
+    try_retrieve(
+        "https://github.com/MDIL-SNU/SevenNet/raw/dff008ac9c53d368b5bee30a27fa4bdfd73f19b2/sevenn/pretrained_potentials/SevenNet_l3i5/checkpoint_l3i5.pth",
+        args.path / "SevenNet_l3i5.pth",
     )
--- tests/utils.py
+++ tests/utils.py
@@ -199,3 +199,15 @@ def chdir(path):
         yield
     finally:
         os.chdir(prev_cwd)
+
+
+def rename_atoms_attributes(
+    atoms: Atoms, rename_info: dict[str, str], rename_arrays: dict[str, str]
+) -> None:
+    """Rename an Atoms objects info and arrays entries in place."""
+    for name_map, store in zip(
+        (rename_info, rename_arrays), (atoms.info, atoms.arrays), strict=True
+    ):
+        for old_name, new_name in name_map.items():
+            if old_name in store:
+                store[new_name] = store.pop(old_name)
